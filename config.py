e2b1933bde8db22a237ee6d40b8c7bf1058a755d
agtiryaki <35417592+agtiryaki@users.noreply.github.com>
2018-01-14 11:06:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # Get an API and Secret Key 
 # https://www.binance.com/restapipub.html
 
-api_key = ''
-api_secret = ''
+api_key = 'UAqCjsI4kqXmt9EPtrJlzJTu1L4DYb7FsahKD6QsrFd8yWiMUXNNDf4mZw4uf1UW'
+api_secret = 'LH63QwKs9a64xm5LpIzFxzGZr3yd2sxXVg1jTBsCRpE4oqgvzoebjYV98Rk3x3bf'
 
 recv_window = 6000000
